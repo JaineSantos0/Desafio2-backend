956d9e0cb100e7f41440972a878f5a093f5b7203
Jaine Santos <jainejosiane@gmail.com>
2023-12-14 14:17:02 -0300

    Create Block

---
+++ src/main/java/com/jainejosiane/desafio2/entities/Block.java
@@ -0,0 +1,64 @@
+package com.jainejosiane.desafio2.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_block")
+public class Block {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant start;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant end;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
+
+    public Block() {
+    }
+
+    public Block(Long id, Instant start, Instant end, Activity activity) {
+        this.id = id;
+        this.start = start;
+        this.end = end;
+        this.activity = activity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getStart() {
+        return start;
+    }
+
+    public void setStart(Instant start) {
+        this.start = start;
+    }
+
+    public Instant getEnd() {
+        return end;
+    }
+
+    public void setEnd(Instant end) {
+        this.end = end;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+}
